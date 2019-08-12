75928c1c3d7336ae80c8133c03f49d9e6e63c060
Anthony Dmytryshyn <admytrys@ualberta.ca>
2019-08-12 12:03:19 -0600

    Burger

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 var animation = bodymovin.loadAnimation({
-    container: document.getElementById('bm'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'data3.json'
+  container: document.getElementById('bm'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'data3.json'
+})
+
+const navSlide = () => {
+  const burger = document.querySelector('.burger')
+  const nav = document.querySelector('.nav-links')
+  const navLinks = document.querySelectorAll('nav-links li')
+
+  //Toggle nav
+  burger.addEventListener('click', () => {
+
+    nav.classList.toggle('nav-active')
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = ''
+      } else {
+        link.style.animation = 'navLinkFade 0.5s ease forwards ${index /7 + 0.3}s'
+      }
+
+    })
+
+    // Burger Animation
+      burger.classList.toggle('toggle');
   })
+
+
+}
+
+navSlide()
